811fed3270352f4365c12da8917ddf67a0539af2
Hadi Hassan <hadihassan40@gmail.com>
2020-12-17 01:24:07 +0200

    fixed l sharmouta aanjad

--- client/src/api/petition.api.ts
+++ client/src/api/petition.api.ts
@@ -1,40 +1,40 @@
 import axios from "axios";
 
 export const getPetitions = async () => {
-  return axios.get("https://app-299.herokuapp.com/api/petition");
+  return axios.get("https://api-299.herokuapp.com/api/petition");
 };
 
 export const getPetitionsByStudent = async (email: String) => {
-  return axios.get("https://app-299.herokuapp.com/api/petition/" + email);
+  return axios.get("https://api-299.herokuapp.com/api/petition/" + email);
 };
 
 export const createPetition = async (body: any) => {
-  return axios.post("https://app-299.herokuapp.com/api/petition", body);
+  return axios.post("https://api-299.herokuapp.com/api/petition", body);
 };
 
 export const updatePetition = async (body: any, id: String) => {
   return axios.post(
-    "https://app-299.herokuapp.com/api/petition/edit/" + id,
+    "https://api-299.herokuapp.com/api/petition/edit/" + id,
     body
   );
 };
 
 export const deletePetition = async (id: String) => {
-  return axios.get("https://app-299.herokuapp.com/api/petition/remove/" + id);
+  return axios.get("https://api-299.herokuapp.com/api/petition/remove/" + id);
 };
 
 export const createStudentGrades = async (body: any) => {
-  return axios.post("https://app-299.herokuapp.com/api/transcript", body);
+  return axios.post("https://api-299.herokuapp.com/api/transcript", body);
 };
 
 export const getStudentGrades = async (id: String) => {
-  return axios.get("https://app-299.herokuapp.com/api/transcript/" + id);
+  return axios.get("https://api-299.herokuapp.com/api/transcript/" + id);
 };
 
 export const getRules = async () => {
-  return axios.get("https://app-299.herokuapp.com/api/rules");
+  return axios.get("https://api-299.herokuapp.com/api/rules");
 };
 
 export const postRule = async (body: any) => {
-  return axios.post("https://app-299.herokuapp.com/api/rules", body);
+  return axios.post("https://api-299.herokuapp.com/api/rules", body);
 };
