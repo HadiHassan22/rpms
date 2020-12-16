0e81e22484e039286feeed890a7587007a78fcb9
Hadi Hassan <hadihassan40@gmail.com>
2020-12-17 01:04:26 +0200

    added cors

--- server/server.js
+++ server/server.js
@@ -6,15 +6,17 @@ const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const apis = require("./api");
 app.use("/api", apis);
 
-
-mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose
+  .connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected..."))
   .catch((err) => console.log(err));
 
